bde5519a1107190c503ba8bbb2a8bf9e02302111
Blas Mena <blasmena@gmail.com>
2019-08-08 19:50:18 -0400

    Se crea prefab de katana

---
+++ Obstaculos/Assets/Scripts/ResetPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPlayer : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.position = Vector3.zero;
+            other.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.transform.position = Vector3.zero;
+            collision.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+}
